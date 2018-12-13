3f5f5a5c0c2656f690977a1a927dd7054f016180
NanarKeurkunian <45368469+NanarKeurkunian@users.noreply.github.com>
2018-12-12 20:18:38 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup(){
 	createCanvas(windowWidth,windowHeight)
 	background(random(255),random(255),random(255))
 	
-	text(["CLICK THROUGH"],300,300)
+	text(["CLICK-THROUGH"],300,300)
 	
 	for(var i=0; i<Celebs.length; i++){
 		Celebs[i].resize(200,0)
